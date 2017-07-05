92dd2be753b8e18a5524eb2475caa55a7558d159
DDAndyChen <andy@deskdirector.com>
2017-07-05 16:52:05 +1200

    Add FlowOperationDescriptionAttribute support

---
+++ src/NSwag.Annotations/FlowOperationDescriptionAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NSwag.Annotations
+{
+    /// <summary>Title of the entity for x-ms-summary. Example - 'Task Name', 'Due Date', etc. It is recommended that you use title case.</summary>
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Parameter)]
+    public class FlowOperationDescriptionAttribute : Attribute
+    {
+        /// <summary>Initializes a new instance of the <see cref="FlowOperationDescriptionAttribute"/> class.</summary>
+        /// <param name="description">The description.</param>
+        public FlowOperationDescriptionAttribute(string description)
+        {
+            Description = description;
+        }
+
+        /// <summary>Gets or sets the description.</summary>
+        public string Description { get; set; }
+    }
+}
---
+++ src/NSwag.SwaggerGeneration.WebApi/Processors/FlowProcessor.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using NJsonSchema.Infrastructure;
+using NSwag.SwaggerGeneration.Processors;
+using NSwag.SwaggerGeneration.Processors.Contexts;
+
+namespace NSwag.SwaggerGeneration.WebApi.Processors
+{
+    /// <summary>Flow Processor</summary>
+    public class FlowProcessor : IOperationProcessor
+    {
+        /// <summary>Processes the specified method information.</summary>
+        /// <param name="context"></param>
+        /// <returns>true if the operation should be added to the Swagger specification.</returns>
+        public async Task<bool> ProcessAsync(OperationProcessorContext context)
+        {
+            await AddDescription(context).ConfigureAwait(false);
+
+            return true;
+        }
+
+        private async Task AddDescription(OperationProcessorContext context)
+        {
+            dynamic flowDescAttr = context.MethodInfo.GetCustomAttributes()
+                .SingleOrDefault(a => a.GetType().Name == "FlowOperationDescriptionAttribute");
+
+            if (flowDescAttr != null)
+                context.OperationDescription.Operation.Description = flowDescAttr.Description;
+            else {
+                var remarks = await context.MethodInfo.GetXmlRemarksAsync().ConfigureAwait(false);
+                if (remarks != string.Empty)
+                    context.OperationDescription.Operation.Description = remarks;
+            }
+        }
+    }
+}
